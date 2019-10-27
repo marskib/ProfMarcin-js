297460a619b62c38936276a225533b36f02a7c90
marskib <tata.marcina@autyzmsoft.pl>
2019-10-27 15:05:25 +0100

    efekty....

--- plansza.js
+++ plansza.js
@@ -5,8 +5,14 @@ let btns = [];       //tablica z klawiszami (referencje only)
 let NROBR = null;    //numer wylosowanego obrazka
 let pctName = "";    //nazwa wylosowanego obrazka
 let pctArea = null;  //miejsce na planszy (div) na obrazek
-let bDalej  = null;  //klawisz bDalej
-let PODP = null;     //czy z Podpowiedzia
+let hintArea = null; //obszar na podpowiedz i/lub duzą nazwę obrazka (po Zwyciestwie)
+//inicjalne wartosci obszaru podpowiedzi/duzej nazwy obrazka:
+let hAColorInit = "";
+let hAFSizeInit = "";
+//
+let bDalej = null;  //klawisz bDalej
+let PODP = null;  //czy z Podpowiedzia
+const PCT_DELAY = 1000; //opoznienie w pokazywaniu obrazka
 
 let wyrazy = ["bluzka", "chleb", "choinka", "cukierki", "czajnik", "czekolada", "dziewczynka", "długopis", "grzebień", "jabłko",
     "klocki", "kot", "kredki", "krzesło", "książka", "lampa", "miotła", "miś", "myszka", "młotek", "nożyczki", "nóż", "odkurzacz", "okno",
@@ -25,31 +31,51 @@ window.onload = Inicjacja;//---------------------------//
 
 function Inicjacja() {
     pctArea = document.getElementById("pctArea"); //uchwyt do obrazka
-    bDalej  = document.getElementById("bDalej");  //uchwyt do klawisza bDalej
+    bDalej = document.getElementById("bDalej");  //uchwyt do klawisza bDalej
+    hintArea = document.getElementById("hintArea");//uchwyt do hintArea
+    //inicjalne wartosci obszaru podpowiedzi/duzej nazwy:
+    var robStyle = getComputedStyle(hintArea);
+    hAColorInit = robStyle.color;
+    hAFSizeInit = robStyle.fontSize;
+    //
     pobierzParametry();
     NROBR = ustawObrazek();
     utworzKlawisze();
     obdzielKlawisze(NROBR);
     wyswietlPodpowiedz();
-    ukryjElement(bDalej);
 }
-
-
 
 function wyswietlPodpowiedz() {
     if (!PODP) return;
-    document.getElementById("hintArea").innerHTML = "<p>" + pctName + "</p>";
+    setTimeout(() => (hintArea.innerHTML = "<p>" + pctName + "</p>"), 3 * PCT_DELAY);
+}
+
+function czyscPodObrazkiem() {
+    //-------------------------------------------------
+    //Czysci wszystko pod obrazkiem, czyli
+    //duzy napis i podpowiedz (if any).
+    //-------------------------------------------------
+    //Czyszczenie starej podpowiedzi (if any) i/lub duzego napisu:
+    hintArea.innerHTML = "<p></p>";
+    //Przywrocenie wlasciwosci pod kolejna podpowiedz (if any):
+    hintArea.style.color = hAColorInit;
+    hintArea.style.fontSize = hAFSizeInit;
 }
 
 function ustawObrazek() {
     //---------------------------    
-    //Funckcja z efektem ubocznym ;):
+    //Funckcja z efektem ubocznym ;) :
+    //0.Czysci stary obrazek (efekciarstwo)
     //1.Losuje i wyswietla obrazek
-    //2.Zwraca numer wylosowanego obrazka    
+    //2.Zwraca numer wylosowanego obrazka (efekt uboczny, b.wazny!!!)   
     //---------------------------
+    //czyszczenie starego (dla efektu)
+    pctArea.style.backgroundImage = "url(zasoby/tlo.jpg)";
+    //losowanie obrazka:
     var numob = getRandomIntInclusive(0, wyrazy.length - 1);
     pctName = wyrazy[numob];
-    pctArea.style.backgroundImage = "url(zasoby/" + pctName + ".jpg)";
+    //Pokazanie po lekkim opoznieniu (efekciarstwo):
+    setTimeout(() => (pctArea.style.backgroundImage = "url(zasoby/" + pctName + ".jpg)"), PCT_DELAY);
     return numob;
 }
 
@@ -66,11 +92,12 @@ function pobierzParametry() {
 
 function dajNextExercise() {
     likwidujKlawisze();
+    czyscPodObrazkiem();
     NROBR = ustawObrazek();
-    setTimeout(utworzKlawisze, 800);
-    setTimeout(obdzielKlawisze, 900, NROBR);
+    setTimeout(utworzKlawisze, 2 * PCT_DELAY);
+    setTimeout(obdzielKlawisze, 2.1 * PCT_DELAY, NROBR); //MUSI(!) byc nieco pozniej
     wyswietlPodpowiedz();
-    ukryjElement(bDalej);
+    ukryjbDalej();
 }
 
 function utworzKlawisze() {
@@ -92,7 +119,7 @@ function obdzielKlawisze(pictMustBe) {
     var kl_wylos = getRandomIntInclusive(0, LKL - 1);
     var wyr_wysw = wyrazy[pictMustBe]; //wyraz wylosowany (=wyswietlany)
     btns[kl_wylos].innerHTML = "<p>" + wyr_wysw + "</p>";
-    btns[kl_wylos].style.color = "maroon";
+    //btns[kl_wylos].style.color = "maroon";
     //Wszystkie inne klawisze, (za wykatkiem wylosowanego i obsluzonego powyzej), dostaja losowe nazwy:
     btns.forEach((elem, i) => { if (i !== kl_wylos) { elem.innerHTML = "<p>" + dajWyrazRandom(wyr_wysw) + "</p>" }; });
 }
@@ -109,7 +136,7 @@ function dajWyrazRandom(notAllowed) {
         //'trick' - zabezpieczenie przed zapetleniem przy malych liczbach wyrazow/obrazkow = wyrazy[];
         //mozliwe jest jednak uzyskanie takiego samego wyrazu...(im wieksza liczba w warunku, tym pniejsze p-stwo)
         i++;
-        if (i>10) break; 
+        if (i > 10) break;
     } while (wyrazy[idx] === notAllowed);
     return wyrazy[idx];
 }
@@ -139,20 +166,47 @@ function likwidujKlawisze() {
 
 function handleKlikOnKlawisz(event) {
     var tekst = event.target.innerText;
-    if (tekst===pctName) {
-        // alert("TRAFILES!!!");
-        pokazElement(bDalej);
+    if (tekst === pctName) {
+        Zwyciestwo();
     }
 }
 
-function ukryjElement(elem){
-    elem.style.display = "none";
+function ukryjbDalej() {
+    bDalej.style.backgroundColor = "transparent";
+    bDalej.style.color = "transparent";
 }
 
-function pokazElement(elem){
-//najprostsza forma, nie sprawdzam jaki byl przedtem, nieelegancko...    
-    elem.style.display = "block"; 
+
+function Zwyciestwo() {
+    //---------------------------------------
+    //Czynnosci po poprawnym odgadnieciu klawisza
+    //---------------------------------------
+    pokazNapis();
+    pokazbDalej(300);
+
 }
+
+function pokazNapis() {
+    //--------------------------------
+    //Pod obrazkiem, w miejscu podpowiedzi (if any), pokazuje sie duzy napis.
+    //--------------------------------
+    hintArea.innerHTML = "<p>" + pctName + "</p>";
+    hintArea.style.color = "maroon";
+    var fsString = hAFSizeInit.substr(0, 2);
+
+    if (pctName.length > 7)
+        hintArea.style.fontSize = 1.5 * fsString + "px";
+    else
+        hintArea.style.fontSize = 2.0 * fsString + "px";
+}
+
+function pokazbDalej(delay) {
+    setTimeout(() => {
+        bDalej.style.backgroundColor = "green";
+        bDalej.style.color = "black";
+    }, delay);
+}
+
 
 
 function getRandomIntInclusive(min, max) {
