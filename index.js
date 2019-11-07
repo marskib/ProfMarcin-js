d1d9b333df4f1e8655f0447b87ec0f50dcd1ba50
marskib <tata.marcina@autyzmsoft.pl>
2019-11-07 08:39:50 +0100

    min. refactoring

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function przejmijParametry() {
 
 function dajWartoscRadio(RGrupa) {
    var radios = document.getElementsByName(RGrupa);
-    console.log("radios: ",radios);
+    // console.log("radios: ",radios);
     for (var i = 0, length = radios.length; i < length; i++) {
         if (radios[i].checked) {
             return radios[i].value;
